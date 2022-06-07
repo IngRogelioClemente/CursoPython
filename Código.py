319c8366723fad8c1e72394bf18f9161a9c3a68e
Rogelio Clemente Balderas <rogelioclemente23@hotmail.com>
2022-06-07 15:00:23 -0500

    initial commit

---
+++ Código.py
@@ -0,0 +1,15 @@
+#Programa que imprime el producto de un numero aleatorio del 1 al 10 por un número generado por el usuario.
+import random 
+
+print("Introduzca un numero")
+
+num = int(input("Numero: "))
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+rand = random.choice(lista)
+
+def operacion(num_user, num_random):
+    producto = num_user * num_random
+    return producto
+    
+resultado_final = operacion(num, rand)
+print("Su número", num, "multplicado por el numero aleatorio", rand, "da como resultado:", resultado_final)
